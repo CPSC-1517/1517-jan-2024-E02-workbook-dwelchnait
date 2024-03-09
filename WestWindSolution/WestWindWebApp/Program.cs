8ccb8345487e5d839c4e1c3a416507d2b092d5c9
Don Welch <dwelch@nait.ca>
2024-03-08 20:51:17 -0700

    setup and consume a service in the web application

--- WestWindSolution/WestWindWebApp/Program.cs
+++ WestWindSolution/WestWindWebApp/Program.cs
@@ -1,6 +1,19 @@
 using WestWindWebApp.Components;
+using WestWindSystem;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//retrievve the connection string the appsettings
+//the connection string will be passed to the class library extension method
+//  for use in registering the access to the residered database
+var connectionstring = builder.Configuration.GetConnectionString("WWDB");
+
+//setup the registraton of services to be available for use by this web application
+//the technique used in this example has the registration encapsulated within the
+//  class library extension class
+//technically, you could do all the setup within this file
+builder.Services.WestWindExtensionServices(options => options.UseSqlServer(connectionstring));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
