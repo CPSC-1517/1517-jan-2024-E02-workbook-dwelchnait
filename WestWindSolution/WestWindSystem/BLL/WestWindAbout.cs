6ab6b00a73dffa30332e39d767b06c0ba8420746
Don Welch <dwelch@nait.ca>
2024-03-08 20:00:32 -0700

    setup extension class for registering services in the class library

---
+++ WestWindSolution/WestWindSystem/BLL/WestWindAbout.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class WestWindAbout
+    {
+        #region setup of the context connecton variable and class constructor
+        private readonly WestWindContext _context;
+
+        //this constructor is obtaining the instance to the WestWindContext class
+        //  that was added in the WestWindExtension class via the registrant of my context clas
+        //  in the WestWindExtensionServices method
+        internal WestWindAbout(WestWindContext syscontext )
+        {
+            _context = syscontext;
+        }
+        #endregion
+
+        //Services
+        #region Queries
+        public BuildVersion WestWindAbout_Get()
+        {
+            //call the context property and apply the LINQ query to the dataset (DbSet<T>) returned
+            //  from the database.
+            //each property is tried to a specific table in your database and is reflected by the
+            //  property name
+            IEnumerable<BuildVersion> info = _context.BuildVersions;
+
+            //return one row from the data set within info. 
+            //.FirstOrDefault() takes the first instance in the dataset. If the dataset is empty
+            //  the returned value will be null
+            return info.FirstOrDefault();                                
+        }
+        #endregion
+    }
+}
---
+++ WestWindSolution/WestWindSystem/WestWindExtensions.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WestWindSystem.BLL;
+using WestWindSystem.DAL;
+#endregion
+
+namespace WestWindSystem
+{
+    public static class WestWindExtensions
+    {
+        //this is a method of the extension class which will be call from the
+        //   Program.cs file of the Web Application
+        //this method will do 2 things
+        //  register the context connection string
+        //  register ALL services that I create within the BLL layer classes
+        public static void WestWindExtensionServices(this IServiceCollection services,
+                Action<DbContextOptionsBuilder> options)
+        {
+            //handle the connection string
+            //add my context class to the services (IServiceCollection)
+            services.AddDbContext<WestWindContext>(options);
+
+            //register ANY BLL service class
+            //to register a service class you will use the IServiceCollection method .AddTransient<T>
+            //for any outside coding that requires access to one of my coded service
+            //      you MUST register the service
+            services.AddTransient<WestWindAbout>((serviceProvider) =>
+                {
+                    var context = serviceProvider.GetService<WestWindContext>();
+                    //create an instance of the service class and register said class in IServiceCollection
+                    //once the class has been registered, it can be used by ANY outside source of your class library
+                    return new WestWindAbout(context);
+                });
+        }
+    }
+}
